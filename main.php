39bdb1c1b45f8ae137434a171e9e40de1e7acd08
Basti <51531217+crftwrk@users.noreply.github.com>
2026-06-19 10:22:41 +0200

    Update plugin version and change action hook

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /*Plugin Name: Update Plugin Public Repo
 Plugin URI: https://bootscore.me/documentation/
 Description: Updates from GitHub Releases
-Version: 0.2.0
+Version: 0.3.0
 Tested up to: 7.0
 Requires at least: 5.0
 Requires PHP: 7.4
@@ -25,6 +25,6 @@ require_once plugin_dir_path(__FILE__) . 'inc/updater-config.php';
 function add_alert_after_primary_open_1() {
   echo '<div class="alert alert-info" role="alert">Update Plugin Public Repo is installed!</div>';
 }
-add_action('bootscore_after_primary_open', 'add_alert_after_primary_open_1');
+add_action('wp_body_open', 'add_alert_after_primary_open_1');
 
 
